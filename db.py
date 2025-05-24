6c1f36f24f26cc1bcb9b4f54ecfabfc74b0bd8c3
shubham-32165 <shubhpk321@gmail.com>
2025-05-24 19:40:23 +0530

    Delete db.py

--- db.py
+++
@@ -1,9 +0,0 @@
-from flask_pymongo import PyMongo
-from flask_mysqldb import MySQL
-from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager
-
-mongo = PyMongo()
-mysql = MySQL()
-bcrypt = Bcrypt()
-jwt = JWTManager()
